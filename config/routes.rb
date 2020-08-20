7a25798c715b96c08328ba97e9c387053c254150
jorry-git <67242361+jorry-git@users.noreply.github.com>
2020-08-20 20:06:21 +0900

    create logout-page
    ログアウトページの作成。
    ログアウト後の動作については未実装。

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'products#index'
   get 'products/my_page'
+  get 'products/logout'
   resources :products, only: [:index, :new, :create]
   resources :orders, only: :show
 end
