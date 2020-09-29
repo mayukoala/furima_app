0d6b8613c12e2c90992fe93d87163a3a4dc21b5c
yumiki06 <65440153+yumiki06@users.noreply.github.com>
2020-09-30 01:15:08 +0900

    Fix permit

--- app/controllers/new_user_controller.rb
+++ app/controllers/new_user_controller.rb
@@ -2,10 +2,9 @@ class NewUserController < ApplicationController
   def index
     @users = User.all
   end
-
+  
   private
-
   def user_params
-    params.require(:user).permit(:nickname, :email)
+    params.require(:user).permit(:nickname,:family_name,:first_name,:family_name_kana,:first_name_kana,:email,:birthday,:password)
   end
 end
