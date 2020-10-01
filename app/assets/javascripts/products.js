1727887ad88a9c39cae8e9af783dc79a5cb1b57e
mayukoala <mayu.3104.j@gmail.com>
2020-10-01 14:36:58 +0900

    Add js of Drag and drop
    ドラックアンドドロップで画像を追加できるようにjsを追加。
    大きなデザイン崩れはないが、追加された画像プレビューのデザインの調整が必要

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -164,6 +164,132 @@ $(function()  {
       $('.btn_contents').css('padding','25px')
     }
   });
+  
+  //ドラッグアンドドロップ
+  //領域に入ったとき
+  $(document).on('dragenter', ".input", function(){
+    $(".input").css('border', '1px solid greenyellow');
+  });
+  //領域から出たとき
+  $(document).on('dragleave', ".input", function(){
+    $(".input").css('border', '1px dashed rgb(204, 204, 204)');
+  });
+  //領域上にあるとき
+  $(document).on('dragover', ".input", function(e){
+    e.preventDefault();
+  });
+  // ドロップした時
+  $(document).on('drop', ".input", function(e){
+    e.preventDefault();
+    let file = e.originalEvent.dataTransfer.files[0];
+    let reader = new FileReader();
+    reader.readAsDataURL(file);
+    $(".input-area").children('.image_upload')[0].files = e.originalEvent.dataTransfer.files;
+    
+    // ファイル形式を画像だけに制限
+    if (!file.type.match('image.*')) {
+      alert('画像を選択してください');
+      return;
+    }
+    $ul = $("#previews");
+    $li = $("#previews li").last();
+    $label = $(this).children(".upload-label");
+    $inputs = $ul.find(".image_upload");
+
+    //$liに追加するためのプレビュー画面のHTML
+    var preview = $(
+      `<div class="image-preview__wapper">
+      <img class="preview">
+      <div class="image-preview_btn">
+      <div class="image-preview_btn_delete">削除</div>
+      </div>
+      </div>`
+    );
+    //次の画像を読み込むためのinput。処理の最後にappendで追加する。
+    var append_input = $(
+      `<li class="input">
+      <label class="upload-label">
+      <div class="btn_contents">
+      <i class="fas fa-camera fa-2x"></i>
+      <div class="upload-label__text">
+      クリックしてファイルをアップロード
+      </div>
+      </div>
+      <div class="input-area">
+      <input class="hidden image_upload" type="file">
+      </div>
+      </label></li>`
+    );
+     reader.onload = function (e) {
+       
+        //previewをappendで追加する前に、プレビューできるように画像を<img>に'src'で付与させる
+        $(preview).find(".preview").attr("src", e.target.result);
+      };
+
+      //inputの画像を付与した,previewを$liに追加。
+      $li.append(preview);
+
+      //プレビュー完了後は、inputを非表示にさせる。これによりプレビューだけが残る。
+      $label.css("display", "none");
+      $li.removeClass("input");
+      $li.addClass("image-preview");
+      $lis = $ul.find('.image-preview');
+    
+      $("#previews li").css({
+        width: `120px`,
+      });
+      //"ul"に新しい"li(inputボタン)"を追加させる。
+      
+      if ($lis.length <= 3 ){
+        $ul.append(append_input)
+        $('#previews li:last-child').css({
+          'width': `calc(100% - (120px * ${$lis.length}))`
+        })
+      }
+      else if( $lis.length == 4 ){
+        $ul.append(append_input)
+        $('#previews li:last-child').css({
+          'width': `calc(100% - (120px * ${$lis.length}))`
+        })
+        $('.btn_contents').css('padding','25px')
+      }
+
+      else if( $lis.length == 5 ){
+        $li.addClass('image-preview');
+        $ul.append(append_input)
+        $('#previews li:last-child').css({
+          'width': `100%`
+        })
+      }
+      // ９個のプレビューのとき、1個のinputを追加。最後の数は9です。
+      else if($lis.length <= 9 ){
+        $li.addClass('image-preview');
+        $ul.append(append_input)
+        $('#previews li:last-child').css({
+          'width': `calc(100% - (120px * (${$lis.length} - 5 )))`
+        })
+        $('.btn_contents').css('padding','25px')
+      }
+
+
+      //inputの最後の"data-image"を取得して、input nameの番号を更新させてる。
+      $inputs.each(function (num, input) {
+        //nameの番号を更新するために、現在の番号を除去
+        $(input).removeAttr("name");
+        $(input).attr({
+          name: "product[product_images_attributes][" + num + "][image_url]",
+          id: "product_product_images_attributes_" + num + "_image_url",
+        });
+      });
+    
+  });
+
+
+
+
+
+
+
 
   // 価格入力時に手数料、利益計算
   $('.input_style-price').on('keyup', function(){
@@ -180,5 +306,8 @@ $(function()  {
       $('.result').html(profit);
     }
   });
+
+  
+
 });
 
